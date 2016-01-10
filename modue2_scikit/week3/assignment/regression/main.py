328a0bd8ee66eb1d1ac464576a6ec9c87a5acd91
Kishwar <kishwarkumar@Kishwars-MacBook-Pro.local>
2016-01-10 20:31:09 +0100

    week3 updates..

---
+++ modue2_scikit/week3/assignment/regression/main.py
@@ -0,0 +1,13 @@
+# These are my assignment project codes developed in python.
+# Anyone can use these codes without any permission
+#
+# Contact info: Kishwar Kumar [kumar.kishwar@gmail.com]
+# Country: France
+#
+
+
+
+__author__ = 'kishwarkumar'
+__date__ = '10/1/16' '8:29 PM'
+
+if __name__ == "__main__":
